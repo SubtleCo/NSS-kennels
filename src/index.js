882ed8e5af5ecc11ca93807fb8ff64eda6e5e44a
Alex Martin <alex@subtleco.co>
2021-02-25 15:17:04 -0600

    Set up Navbar and React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Kennel } from "./components/Kennel"
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Kennel />
+    <Router>
+      <Kennel />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
