48436dae6ae7314b344aaa00bee7406cdd5c5f7d
Alex Martin <alex@subtleco.co>
2021-03-02 11:31:05 -0600

    Added Location Detail view

---
+++ src/components/location/LocationDetail.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+import { LocationContext } from './LocationProvider'
+
+export const LocationDetail = () => {
+    const { getLocationById } = useContext(LocationContext)
+    const [location, setLocation] = useState({})
+    const history = useHistory()
+    const { locationId } = useParams()
+
+    useEffect(() => {
+        getLocationById(locationId)
+        .then(setLocation)
+    }, [])
+
+    return (
+        <section className="location">
+            <h2 className="location__name">{location.name}</h2>
+            <address className="location__address">{location.address}</address>
+            <h4>Employees</h4>
+            <p className="location__employees">
+                {
+                    location.employees?.map(e => e.name).join(", ")
+                }
+            </p>
+            <h4>Current Residents</h4>
+            <p className="location__animals">
+                {
+                    location.animals?.map(a => a.name).join(", ")
+                }
+            </p>
+        </section>
+    )
+}
